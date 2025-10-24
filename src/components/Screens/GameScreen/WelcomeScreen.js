f3792f2144471286f9903a40248961520630b1c4
pramod ade <prmdextracthere@gmail.com>
2025-10-24 16:08:42 +0530

    completed

---
+++ src/components/Screens/GameScreen/WelcomeScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const WelcomeScreen = (props) => {
+  const gameModeChange = (e) => {
+    props.handleModeChange(Number(e.target.value));
+  };
+
+  return (
+    <>
+      <h5>Welcome!</h5>
+      <div onChange={gameModeChange} className='levels_container'>
+        <input id="easy" type="radio" value="8" name="gameMode" />
+        <label htmlFor="easy"> Easy</label>
+
+        <input id="normal" type="radio" value="16" name="gameMode" />
+        <label htmlFor="normal"> Normal</label>
+
+        <input id="hard" type="radio" value="24" name="gameMode" />
+        <label htmlFor="hard"> Hard</label>
+      </div>
+      <button onClick={props.newGameClick}>Start</button>
+    </>
+  );
+};
+
+export default WelcomeScreen;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./store/reducers/index";
+import MemoryGameContainer from "./container/MemoryGameContainer/MemoryGameContainer";
 
+// Create store using Redux Toolkit
+const store = configureStore({
+  reducer: rootReducer,
+});
 
+function App() {
+  return (
+    <Provider store={store}>
+      <MemoryGameContainer />
+    </Provider>
+  );
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
